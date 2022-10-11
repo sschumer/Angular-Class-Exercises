7bdf29a6ed6449fd8c78fc6153efb027381312db
Sara Schumer <sac.s@sbcglobal.net>
2022-10-11 18:46:10 -0500

    Class Thirteen Exercises
    Data binding

---
+++ ClassThirteen-Ex/angular-nested-components-input-exercise/src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent implements OnInit {
+  @Input() arraysPosts: string[] = []
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.arraysPosts);
+
+  }
+
+}
